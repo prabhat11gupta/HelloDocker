51024631addc6cf338764cb5d0cda1ccb95402df
Prabhat Gupta <prabhat11gupta@gmail.com>
2026-08-30 06:21:29 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello from Dockerized .NET Console App!");
+Console.WriteLine($"Running on: {Environment.OSVersion}");
+
